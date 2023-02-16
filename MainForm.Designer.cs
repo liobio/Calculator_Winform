ff94f5f40f5669b6276795b463cae48247b466d4
liobio <582437980@qq.com>
2023-02-16 22:44:26 +0800

    fix window shadow when from show

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.KeyPanel = new System.Windows.Forms.Panel();
+            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.FormulaPanel = new System.Windows.Forms.Panel();
+            this.HistoryPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.HistoryBtn = new System.Windows.Forms.Button();
             this.TopPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.MinBtn = new System.Windows.Forms.Button();
             this.MaxBtn = new System.Windows.Forms.Button();
             this.CloseBtn = new System.Windows.Forms.Button();
-            this.HistoryPanel = new System.Windows.Forms.Panel();
-            this.HistoryBtn = new System.Windows.Forms.Button();
-            this.FormulaPanel = new System.Windows.Forms.Panel();
-            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.MainPanel.SuspendLayout();
+            this.HistoryPanel.SuspendLayout();
             this.TopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.HistoryPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.KeyPanel);
+            this.MainPanel.Controls.Add(this.ResultPanel);
+            this.MainPanel.Controls.Add(this.FormulaPanel);
+            this.MainPanel.Controls.Add(this.HistoryPanel);
+            this.MainPanel.Controls.Add(this.TopPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Padding = new System.Windows.Forms.Padding(1);
+            this.MainPanel.Size = new System.Drawing.Size(384, 640);
+            this.MainPanel.TabIndex = 4;
+            this.MainPanel.MouseLeave += new System.EventHandler(this.Main_Leave);
+            this.MainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            // 
+            // KeyPanel
+            // 
+            this.KeyPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.KeyPanel.Location = new System.Drawing.Point(1, 193);
+            this.KeyPanel.Name = "KeyPanel";
+            this.KeyPanel.Size = new System.Drawing.Size(382, 446);
+            this.KeyPanel.TabIndex = 8;
+            // 
+            // ResultPanel
+            // 
+            this.ResultPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ResultPanel.Location = new System.Drawing.Point(1, 129);
+            this.ResultPanel.Name = "ResultPanel";
+            this.ResultPanel.Size = new System.Drawing.Size(382, 64);
+            this.ResultPanel.TabIndex = 7;
+            // 
+            // FormulaPanel
+            // 
+            this.FormulaPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FormulaPanel.Location = new System.Drawing.Point(1, 65);
+            this.FormulaPanel.Name = "FormulaPanel";
+            this.FormulaPanel.Size = new System.Drawing.Size(382, 64);
+            this.FormulaPanel.TabIndex = 6;
+            // 
+            // HistoryPanel
+            // 
+            this.HistoryPanel.Controls.Add(this.button1);
+            this.HistoryPanel.Controls.Add(this.HistoryBtn);
+            this.HistoryPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HistoryPanel.Location = new System.Drawing.Point(1, 33);
+            this.HistoryPanel.Name = "HistoryPanel";
+            this.HistoryPanel.Size = new System.Drawing.Size(382, 32);
+            this.HistoryPanel.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Calculator.Properties.Resources.History;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(318, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 32);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.TestEventClick);
+            // 
+            // HistoryBtn
+            // 
+            this.HistoryBtn.BackgroundImage = global::Calculator.Properties.Resources.History;
+            this.HistoryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.HistoryBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.HistoryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HistoryBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.HistoryBtn.Location = new System.Drawing.Point(350, 0);
+            this.HistoryBtn.Name = "HistoryBtn";
+            this.HistoryBtn.Size = new System.Drawing.Size(32, 32);
+            this.HistoryBtn.TabIndex = 0;
+            this.HistoryBtn.UseVisualStyleBackColor = true;
+            this.HistoryBtn.Click += new System.EventHandler(this.HistoyrBtnClick);
             // 
             // TopPanel
             // 
@@ -52,10 +134,10 @@
             this.TopPanel.Controls.Add(this.CloseBtn);
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.ForeColor = System.Drawing.SystemColors.Control;
-            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Location = new System.Drawing.Point(1, 1);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(384, 32);
-            this.TopPanel.TabIndex = 0;
+            this.TopPanel.Size = new System.Drawing.Size(382, 32);
+            this.TopPanel.TabIndex = 4;
             this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel_MouseDown);
             // 
             // label1
@@ -86,11 +168,12 @@
             this.MinBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.MinBtn.Dock = System.Windows.Forms.DockStyle.Right;
             this.MinBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.MinBtn.Location = new System.Drawing.Point(246, 0);
+            this.MinBtn.Location = new System.Drawing.Point(244, 0);
             this.MinBtn.Name = "MinBtn";
             this.MinBtn.Size = new System.Drawing.Size(46, 32);
             this.MinBtn.TabIndex = 2;
             this.MinBtn.UseVisualStyleBackColor = true;
+            this.MinBtn.Click += new System.EventHandler(this.Min_Click);
             // 
             // MaxBtn
             // 
@@ -98,94 +181,60 @@
             this.MaxBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.MaxBtn.Dock = System.Windows.Forms.DockStyle.Right;
             this.MaxBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.MaxBtn.Location = new System.Drawing.Point(292, 0);
+            this.MaxBtn.Location = new System.Drawing.Point(290, 0);
             this.MaxBtn.Name = "MaxBtn";
             this.MaxBtn.Size = new System.Drawing.Size(46, 32);
             this.MaxBtn.TabIndex = 1;
             this.MaxBtn.UseVisualStyleBackColor = true;
+            this.MaxBtn.Click += new System.EventHandler(this.Max_Click);
             // 
             // CloseBtn
             // 
             this.CloseBtn.BackgroundImage = global::Calculator.Properties.Resources.Close;
             this.CloseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.CloseBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CloseBtn.Location = new System.Drawing.Point(338, 0);
+            this.CloseBtn.Location = new System.Drawing.Point(336, 0);
             this.CloseBtn.Name = "CloseBtn";
             this.CloseBtn.Size = new System.Drawing.Size(46, 32);
             this.CloseBtn.TabIndex = 0;
             this.CloseBtn.UseVisualStyleBackColor = true;
-            // 
-            // HistoryPanel
-            // 
-            this.HistoryPanel.Controls.Add(this.HistoryBtn);
-            this.HistoryPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.HistoryPanel.Location = new System.Drawing.Point(0, 32);
-            this.HistoryPanel.Name = "HistoryPanel";
-            this.HistoryPanel.Size = new System.Drawing.Size(384, 32);
-            this.HistoryPanel.TabIndex = 1;
-            // 
-            // HistoryBtn
-            // 
-            this.HistoryBtn.BackgroundImage = global::Calculator.Properties.Resources.History;
-            this.HistoryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.HistoryBtn.Dock = System.Windows.Forms.DockStyle.Right;
-            this.HistoryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.HistoryBtn.ForeColor = System.Drawing.SystemColors.Control;
-            this.HistoryBtn.Location = new System.Drawing.Point(352, 0);
-            this.HistoryBtn.Name = "HistoryBtn";
-            this.HistoryBtn.Size = new System.Drawing.Size(32, 32);
-            this.HistoryBtn.TabIndex = 0;
-            this.HistoryBtn.UseVisualStyleBackColor = true;
-            // 
-            // FormulaPanel
-            // 
-            this.FormulaPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.FormulaPanel.Location = new System.Drawing.Point(0, 64);
-            this.FormulaPanel.Name = "FormulaPanel";
-            this.FormulaPanel.Size = new System.Drawing.Size(384, 64);
-            this.FormulaPanel.TabIndex = 2;
-            // 
-            // ResultPanel
-            // 
-            this.ResultPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.ResultPanel.Location = new System.Drawing.Point(0, 128);
-            this.ResultPanel.Name = "ResultPanel";
-            this.ResultPanel.Size = new System.Drawing.Size(384, 64);
-            this.ResultPanel.TabIndex = 3;
+            this.CloseBtn.Click += new System.EventHandler(this.Close_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 640);
-            this.Controls.Add(this.ResultPanel);
-            this.Controls.Add(this.FormulaPanel);
-            this.Controls.Add(this.HistoryPanel);
-            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.MainPanel);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(384, 640);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.MainPanel.ResumeLayout(false);
+            this.HistoryPanel.ResumeLayout(false);
             this.TopPanel.ResumeLayout(false);
             this.TopPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.HistoryPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private Panel MainPanel;
+        private Panel ResultPanel;
+        private Panel FormulaPanel;
+        private Panel HistoryPanel;
+        private Button HistoryBtn;
         private Panel TopPanel;
         private Label label1;
         private PictureBox pictureBox1;
         private Button MinBtn;
         private Button MaxBtn;
         private Button CloseBtn;
-        private Panel HistoryPanel;
-        private Button HistoryBtn;
-        private Panel FormulaPanel;
-        private Panel ResultPanel;
+        private Panel KeyPanel;
+        private Button button1;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,12 @@
 ﻿using Calculator.Properties;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +18,16 @@ namespace Calculator
     public partial class MainForm : Form
     {
         #region Shadow
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-        private static extern IntPtr CreateRoundRectRgn
-          (
-              int nLeftRect, // x-coordinate of upper-left corner
-              int nTopRect, // y-coordinate of upper-left corner
-              int nRightRect, // x-coordinate of lower-right corner
-              int nBottomRect, // y-coordinate of lower-right corner
-              int nWidthEllipse, // height of ellipse
-              int nHeightEllipse // width of ellipse
-           );
+        //[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        //private static extern IntPtr CreateRoundRectRgn
+        //  (
+        //      int nLeftRect, // x-coordinate of upper-left corner
+        //      int nTopRect, // y-coordinate of upper-left corner
+        //      int nRightRect, // x-coordinate of lower-right corner
+        //      int nBottomRect, // y-coordinate of lower-right corner
+        //      int nWidthEllipse, // height of ellipse
+        //      int nHeightEllipse // width of ellipse
+        //   );
 
         [DllImport("dwmapi.dll")]
         public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
@@ -36,9 +38,10 @@ namespace Calculator
         [DllImport("dwmapi.dll")]
         public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
 
-        private bool m_aeroEnabled;                     // variables for box shadow
+        private bool m_aeroEnabled;                     // variables for box shadow 调试获取
         private const int CS_DROPSHADOW = 0x00020000;
         private const int WM_NCPAINT = 0x0085;
+        private const int WM_SHOWWINDOW = 0x18;
         private const int WM_ACTIVATEAPP = 0x001C;
 
         public struct MARGINS                           // struct for box shadow
@@ -80,9 +83,26 @@ namespace Calculator
 
         protected override void WndProc(ref Message m)
         {
+     
             switch (m.Msg)
             {
-                case WM_NCPAINT:                        // box shadow
+                //case WM_NCPAINT:                        // box shadow
+                //    if (m_aeroEnabled)
+                //    {
+                //        var v = 2;
+                //        DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+                //        MARGINS margins = new MARGINS()
+                //        {
+                //            bottomHeight = 1,
+                //            leftWidth = 1,
+                //            rightWidth = 1,
+                //            topHeight = 1
+                //        };
+                //        DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+
+                //    }
+                //    break;
+                case WM_SHOWWINDOW:                        // box shadow
                     if (m_aeroEnabled)
                     {
                         var v = 2;
@@ -153,10 +173,112 @@ namespace Calculator
         }
         #endregion
 
+        #region 拉伸窗口
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            //label2.Text = e.Location.ToString();
+            if (e.Button == MouseButtons.Left)//左键按下移动，拖拽调整大小
+            {
+
+                // MousePosition的参考点是屏幕的左上角，表示鼠标当前相对于屏幕左上角的坐标。this.Left和this.Top的参考点也是屏幕
+                if (Cursor == Cursors.SizeNWSE) // 倾斜拖拽 
+                {
+                    // 改变窗体宽和高的代码，其宽高为鼠标屏幕位置减去窗体的Left，Top距离
+                    this.Width = MousePosition.X - this.Left;
+                    this.Height = MousePosition.Y - this.Top;
+                }
+                else if (Cursor == Cursors.SizeWE) // 水平拖拽
+                {
+                    Width = MousePosition.X - this.Left;
+                }
+                else if (Cursor == Cursors.SizeNS) // 垂直拖拽
+                {
+                    Height = MousePosition.Y - this.Top;
+                }
+                
+            }
+
+            //鼠标移动过程中，坐标时刻在改变 
+            //当鼠标移动时横坐标距离窗体右边缘5像素以内且纵坐标距离下边缘也在5像素以内时，要将光标变为倾斜的箭头形状
+            if (e.Location.X >= this.Width - 5 && e.Location.Y > this.Height - 5)
+            {
+                this.Cursor = Cursors.SizeNWSE; // 右下角 双向对角线光标
+            }
+            //当鼠标移动时横坐标距离窗体右边缘5像素以内时，要将光标变为双向水平箭头形状
+            else if (e.Location.X >= this.Width - 5)
+            {
+                this.Cursor = Cursors.SizeWE; // 双向水平光标
+            }
+            //当鼠标移动时纵坐标距离窗体下边缘5像素以内时，要将光标变为垂直水平箭头形状
+            else if (e.Location.Y >= this.Height - 5)
+            {
+                this.Cursor = Cursors.SizeNS; // 双向垂直光标
+
+            }
+            //否则，以外的窗体区域，鼠标星座均为单向箭头（默认）             
+            else this.Cursor = Cursors.Arrow;
+
+        }
+
+        private void Main_Leave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Arrow;// 移出窗体变为正常
+        }
+        #endregion
+
+        #region 获取控件
+        /// <summary>
+        /// 根据指定容器和控件名字，获得控件
+        /// </summary>
+        /// <param name="obj">容器</param>
+        /// <param name="strControlName">控件名字</param>
+        /// <returns>控件</returns>
+        private object GetControlInstance(object obj, string strControlName)
+        {
+            IEnumerator Controls = null;//所有控件
+            Control c = null;//当前控件
+            Object cResult = null;//查找结果
+            if (obj.GetType() == this.GetType())//窗体
+            {
+                Controls = this.Controls.GetEnumerator();
+            }
+            else//控件
+            {
+                Controls = ((Control)obj).Controls.GetEnumerator();
+            }
+            while (Controls.MoveNext())//遍历操作
+            {
+                c = (Control)Controls.Current;//当前控件
+                if (c.HasChildren)//当前控件是个容器
+                {
+                    cResult = GetControlInstance(c, strControlName);//递归查找
+                    if (cResult == null)//当前容器中没有，跳出，继续查找
+                        continue;
+                    else//找到控件，返回
+                        return cResult;
+                }
+                else if (c.Name == strControlName)//不是容器，同时找到控件，返回
+                {
+                    return c;
+                }
+            }
+            return null;//控件不存在
+        }
+        #endregion
 
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void HistoyrBtnClick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void TestEventClick(object sender, EventArgs e)
+        {
+            this.Show();
         }
     }
 }
